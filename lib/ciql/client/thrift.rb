83b770647d4b123a8450e7cf9c4940c8ba3e8abc
Justin Bradford <justin@nulu.com>
2013-06-21 16:31:19 -0700

    Adding logging options to client.

--- lib/ciql/client/thrift.rb
+++ lib/ciql/client/thrift.rb
@@ -1,9 +1,13 @@
 require 'cassandra-cql/1.2'
 require 'cassandra-cql'
+require 'benchmark'
 
 module Ciql::Client
   class Thrift < CassandraCQL::Database
     def initialize(options={})
+      @logger = options.delete(:log) || Logger.new($stdout)
+      @log_format = options.delete(:log_format) || DEFAULT_LOG_FORMAT
+
       port = options.delete(:port) { 9160 }
       hosts = options.delete(:host) { '127.0.0.1' }.split(',')
       hosts_with_port = hosts.map { |host| [host, port].join(':') }
@@ -14,17 +18,37 @@ module Ciql::Client
       bind_variables = arguments.shift statement.count('?')
       bound_statement = Ciql::Sanitize.sanitize(statement, *bind_variables)
       compression_type = CassandraCQL::Thrift::Compression::NONE
-      consistency_level = CassandraCQL::Thrift::ConsistencyLevel.const_get(
-        (arguments.shift or :quorum).to_s.upcase
+      consistency_level = (arguments.shift or :local_quorum).to_s.upcase
+
+      result = nil
+      times = Benchmark.measure do
+        result = CassandraCQL::Result.new(
+          @connection.execute_cql3_query(
+            bound_statement, compression_type,
+            CassandraCQL::Thrift::ConsistencyLevel.const_get(consistency_level)
+          )
+        )
+      end
+
+      log(
+        duration:    times.real * 10**3,
+        query:       bound_statement,
+        compression: compression_type,
+        consistency: consistency_level
       )
 
-      CassandraCQL::Result.new(
-        @connection.execute_cql3_query(
-          bound_statement, compression_type, consistency_level
-        )
-      )
+      result
+
     rescue CassandraCQL::Thrift::InvalidRequestException
       raise CassandraCQL::Error::InvalidRequestException.new($!.why)
+    end
+
+  private
+    DEFAULT_LOG_FORMAT = '   CQL (%<duration>.3fms)  %{query} (%{consistency})'.freeze
+
+    def log(message)
+      return unless @logger.debug?
+      @logger.debug(@log_format % message)
     end
   end
 end
--- spec/ciql/client/thrift_spec.rb
+++ spec/ciql/client/thrift_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 module Ciql::Client
   describe Thrift do
-    subject { described_class.new }
+    let(:logger) { mock(:debug? => true, :debug => nil) }
+
+    subject { described_class.new(log: logger) }
 
     it 'inherits from CassandraCQL::Database' do
       subject.should be_kind_of CassandraCQL::Database
+    end
+
+    describe '#execute' do
+      it 'logs the query' do
+        subject # instantiate
+        logger.should_receive(:debug).with(/system.local where key = 'local' \(ONE\)/)
+        subject.execute('select * from system.local where key = ?', :local, :one)
+      end
     end
   end
 end
